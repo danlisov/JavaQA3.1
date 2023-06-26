5e5a3a67a77b02ff361b65339a6a067424dc969c
Denis Lysov <dan.lysov2015@yandex.ru>
2023-06-26 19:40:22 +0300

    Initial commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,6 @@ public class BonusMilesService {
     public int calculate(int ticketPrice){
         int bonusPrice = 20;
         int bonus = ticketPrice / bonusPrice;
-
         return bonus;
     }
 }
